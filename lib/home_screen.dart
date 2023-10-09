263a8e71808e0147beb15a40b8b8ebf4b5f4f50c
PeterNat <peternath280@gmail.com>
2023-10-09 19:49:24 +0700

    add home screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,31 +1,163 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Widget cardMenu(String title, String imagePath) {
+      return Card(
+        elevation: 3,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Stack(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height / 5, // Set tinggi Card
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(imagePath),
+                  fit: BoxFit.cover,
+                ),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              child: Container(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  title,
+                  style: const TextStyle(
+                    fontFamily: 'BebasNeue',
+                    fontSize: 32,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+    Widget summerSaleCard() {
+      return Card(
+        color: const Color.fromARGB(255, 209, 182, 157),
+        elevation: 3,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Stack(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height / 5, // Set tinggi Card
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              top: 25,
+              child: Container(
+                padding: const EdgeInsets.all(8.0),
+                child: const Column(
+                  children: [
+                    Text(
+                      'Summer Sale',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: 'BebasNeue',
+                        fontSize: 24,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      '-25%',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: 'BebasNeue',
+                        fontSize: 24,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              child: Container(
+                padding: const EdgeInsets.all(8.0),
+                child: const Text(
+                  'SALE',
+                  style: TextStyle(
+                    fontFamily: 'BebasNeue',
+                    fontSize: 32,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          margin: const EdgeInsets.all(24),
-          child: const Column(
+      // appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Row(
-                children: <Widget>[
-                  Text('Vibe'),
-                  Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Enter a search term',
+                children: [
+                  const Expanded(
+                    flex: 3,
+                    child: Text(
+                      'Vibe',
+                      style: TextStyle(
+                        fontFamily: 'BebasNeue',
+                        fontSize: 32,
+                      ),
                     ),
                   ),
-                ),
+                  Expanded(
+                    flex: 2,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Search',
+                        suffixIcon: IconButton(
+                          icon: const Icon(Icons.qr_code_scanner_rounded),
+                          onPressed: () {},
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
-              )
+              ),
+              const SizedBox(height: 16),
+              cardMenu(
+                'WOMEN',
+                'assets/images/home_women.jpg',
+              ),
+              cardMenu(
+                'MEN',
+                'assets/images/home_men.jpg',
+              ),
+              cardMenu(
+                'CHILDREN',
+                'assets/images/home_children.jpg',
+              ),
+              summerSaleCard(),
             ],
           ),
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -8,16 +8,67 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const Main());
+  }
+}
+
+class Main extends StatefulWidget {
+  const Main({super.key});
+
+  @override
+  State<Main> createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+  int _selectedIndex = 0;
+
+  void _onItemTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter Demo')),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: const <Widget>[
+          HomeScreen(), //widget halaman
+          Text('Index 1: Dashboard'),
+          Text('Index 2: Notifications'),
+          Text('Index 3: Settings'),
+        ],
       ),
-      home: const HomeScreen(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: '',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTap,
+        type: BottomNavigationBarType.fixed,
+      ),
     );
   }
 }
