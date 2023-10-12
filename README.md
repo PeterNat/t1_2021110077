af052322b3664843ce4f1fa87f50b186115fa3e8
PeterNat <peternath280@gmail.com>
2023-10-12 19:04:25 +0700

    add detail product di cart screen

--- README.md
+++ README.md
@@ -1,16 +1,5 @@
-# t1_2021110077
+Link design:
+https://dribbble.com/shots/21695228-Mobile-app-for-clothes-and-shoes
 
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+screenshoots:
+![Alt text](image.png)
--- lib/cart_screen.dart
+++ lib/cart_screen.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 class CartScreen extends StatelessWidget {
@@ -6,42 +7,285 @@ class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 80.0,
-        title: Container(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Row(
-                children: [
-                  const Expanded(
-                    flex: 1,
-                      child: Icon(
-                        Icons.arrow_back_ios_new
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            floating: false,
+            pinned: true,
+            flexibleSpace: AppBar(
+              backgroundColor: Colors.transparent,
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back_ios),
+                color: const Color.fromARGB(255, 0, 0, 0),
+                iconSize: 30.0,
+                onPressed: () {
+                },
+              ),
+              actions: [
+                IconButton(
+                  icon: const Icon(Icons.favorite_border),
+                  color: const Color.fromARGB(255, 0, 0, 0),
+                  iconSize: 30.0,
+                  onPressed: () {
+                  },
+                ),
+              ],
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              const ImageSlider(),
+              const DetailProduct(),
+            ]),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ImageSlider extends StatefulWidget {
+  const ImageSlider({super.key});
+
+  @override
+  _ImageSliderState createState() => _ImageSliderState();
+}
+
+class _ImageSliderState extends State<ImageSlider> {
+  final PageController _controller = PageController();
+  int _currentIndex = 0;
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Stack(
+          alignment: AlignmentDirectional.bottomCenter,
+          children: [
+            SizedBox(
+              height: 300.0,
+              child: PageView(
+                controller: _controller,
+                onPageChanged: (index) {
+                  setState(() {
+                    _currentIndex = index;
+                  });
+                },
+                children: <Widget>[
+                  Image.asset('assets/images/leather_jkt_men1.jpg', fit: BoxFit.cover),
+                  Image.asset('assets/images/leather_jkt_men2.jpg', fit: BoxFit.cover),
+                  // Add more images as needed
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(
+                2, // Change this to the number of images you have
+                (index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: 8.0,
+                      height: 8.0,
+                      margin: const EdgeInsets.symmetric(horizontal: .0),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: _currentIndex == index ? Colors.blue : Colors.grey,
                       ),
                     ),
-                  Expanded(
-                      flex: 5,
-                      child: Icon(
-                        Icons.favorite_border
-                      ),
-                  )
-                ],
-              )
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class DetailProduct extends StatelessWidget {
+  const DetailProduct({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 16),
+          const Text(
+            'Men Jacket',
+            style: TextStyle(
+              fontSize: 12,
+            ),
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Black Leather Jacket',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontFamily: 'BebasNeue',
+                ),
+              ),
+              Text(
+                'Rp. 150.000',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontFamily: 'BebasNeue',
+                ),
+              ),
             ],
           ),
-        ),
-      ),// appBar: AppBar(
-      //   title: Text('Full Screen Image'),
-      // ),
-      body: Column(
-        children: [
-          Image.asset(
-            'assets/images/home_women.jpg', // Ganti dengan path gambar Anda
-            fit: BoxFit.cover,
+          const SizedBox(height: 8.0),
+          const Text(
+            'Voluptate dolor aute ipsum deserunt fugiat enim do fugiat. Mollit consequat ad ex do elit incididunt quis sunt.',
+            style: TextStyle(fontSize: 12.0),
           ),
-          // Tambahkan widget lain di bawah gambar jika diperlukan
+          const Text(
+            'see more',
+            style: TextStyle(
+              fontSize: 12.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 16.0),
+          const Text(
+            'Select size',
+            style: TextStyle(
+              fontSize: 24.0,
+              fontFamily: 'BebasNeue',
+            ),
+          ),
+          const SizedBox(height: 8.0),
+          Row(
+            children: [
+              Card(
+                child: Container(
+                  width: 30.0,
+                  height: 30.0,
+                  alignment: Alignment.center,
+                  child: const Text(
+                    '36',
+                    style:
+                    TextStyle(
+                      fontSize: 12.0,
+                      fontWeight: FontWeight.bold,
+                      ),
+                  ),
+                ),
+              ),
+              Card(
+                child: Container(
+                  width: 30.0,
+                  height: 30.0,
+                  alignment: Alignment.center,
+                  child: const Text(
+                    '38',
+                    style:
+                    TextStyle(
+                      fontSize: 12.0,
+                      fontWeight: FontWeight.bold,
+                      ),
+                  ),
+                ),
+              ),
+              Card(
+                child: Container(
+                  width: 30.0,
+                  height: 30.0,
+                  alignment: Alignment.center,
+                  child: const Text(
+                    '39',
+                    style:
+                    TextStyle(
+                      color: Color.fromARGB(255, 164, 164, 164),
+                      fontSize: 12.0,
+                      fontWeight: FontWeight.bold,
+                      ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 16.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'review [4]',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontFamily: 'BebasNeue',
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    for (int i = 0; i < 5; i++)
+                      const Icon(
+                        Icons.star,
+                      ),
+                  ],
+                ),
+              ),
+              const Icon(
+                Icons.keyboard_arrow_down,
+              ),
+            ],
+          ),
+          const SizedBox(height: 16.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ElevatedButton.icon(
+                icon: const Icon(
+                  Icons.shopping_bag,
+                  color: Color.fromARGB(255, 33, 33, 33),
+                  ),
+                label: const Text(''),
+                onPressed: () {},
+                 style: ButtonStyle(
+                  minimumSize: MaterialStateProperty.all(const Size(50, 50)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0), // Atur radius sudut sesuai kebutuhan
+                    ),
+                  ),
+                  backgroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 240, 240, 240)),
+                 )
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  minimumSize: MaterialStateProperty.all(const Size(200, 50)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  backgroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 42, 42, 42)),
+                 ),
+                child: const Text(
+                  'Go to checkout',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 226, 226, 226),
+                  ),
+                  )
+              ),
+            ],
+          ),
         ],
       ),
     );
